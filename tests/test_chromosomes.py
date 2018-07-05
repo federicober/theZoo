25ef088896a08c2064f3736648e68c1688cc05d9
OBERNDORFER Federico <f.oberndorfer@group-pomona.fr>
2018-07-05 11:35:55 +0200

    get all the same

--- tests/test_chromosomes.py
+++ tests/test_chromosomes.py
@@ -17,14 +17,24 @@ class TestChromosomeClass(unittest.TestCase):
 
 
 class TestBinaryChromosome(unittest.TestCase):
+    @staticmethod
+    def get_all_equal(value: bool, n: int):
+        return Chromosome([BinaryGene(value) for i in range(n)])
+
     def test_equals_two_equal_chromosomes_should_return_true(self):
         self.assertTrue(Chromosome((BinaryGene(True), BinaryGene(False), BinaryGene(False), BinaryGene(True))) ==
                         Chromosome((BinaryGene(True), BinaryGene(False), BinaryGene(False), BinaryGene(True))))
 
     def test_add_two_chromosomes_size_4_all_true_should_return_all_true(self):
-        self.assertEqual(Chromosome((BinaryGene(True), BinaryGene(True), BinaryGene(True), BinaryGene(True))) +
-                         Chromosome((BinaryGene(True), BinaryGene(True), BinaryGene(True), BinaryGene(True))),
-                         Chromosome((BinaryGene(True), BinaryGene(True), BinaryGene(True), BinaryGene(True))))
+        self.assertEqual(self.get_all_equal(True, 4) + self.get_all_equal(True, 4), self.get_all_equal(True, 4))
+
+    def test_add_two_chromosomes_size_4_all_false_should_return_all_false(self):
+        self.assertEqual(self.get_all_equal(False, 4) + self.get_all_equal(False, 4), self.get_all_equal(False, 4))
+
+    def test_add_two_chromosomes_size_4_all_true_and_all_false_should_return_new(self):
+        np.random.seed(42)
+        self.assertEqual(self.get_all_equal(False, 4) + self.get_all_equal(True, 4),
+                         Chromosome((BinaryGene(False), BinaryGene(True), BinaryGene(True), BinaryGene(True))))
 
 
 class TestChromosomeMethods(unittest.TestCase):
